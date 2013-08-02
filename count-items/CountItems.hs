7d007a6a08041f2280281f3977bbc6d0c97e77fb
Eric Rochester <erochest@virginia.edu>
2013-08-02 11:37:27 -0400

    Implemented count-items.

--- count-items/CountItems.hs
+++ count-items/CountItems.hs
@@ -1,7 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 
+import qualified Data.List as L
+import           Data.Monoid
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TIO
+
+
+type Group = (T.Text, Int)
+
+countGroup :: [T.Text] -> Group
+countGroup [x]    = (x, 1)
+countGroup (x:xs) = (x, 1 + length xs)
+
+showGroup :: Group -> T.Text
+showGroup (x, i) = x <> "\t" <> T.pack (show i)
+
 main :: IO ()
-main = putStrLn "count-items"
+main =
+    TIO.interact $ T.unlines . map (showGroup . countGroup) . L.group . T.lines
 
