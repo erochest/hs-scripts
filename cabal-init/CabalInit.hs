52b58ce281197738f4e2efd79d29ef9f3d66a670
Eric Rochester <erochest@virginia.edu>
2013-10-08 14:02:06 -0400

    Added cabal-init script.

---
+++ cabal-init/CabalInit.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ExtendedDefaultRules       #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+
+module Main where
+
+
+import           Data.Maybe (catMaybes)
+
+import           ClassyPrelude hiding ((<>))
+import qualified Data.Text as T
+import           Options.Applicative
+import           Shelly
+
+default (T.Text)
+
+
+cabal_ :: T.Text -> [T.Text] -> Sh ()
+cabal_ = command1_ "cabal" []
+
+cabalSandbox_ :: T.Text -> [T.Text] -> Sh ()
+cabalSandbox_ cmd args = command1_ "cabal" [] "sandbox" $ cmd : args
+
+boolMaybe :: Bool -> a -> Maybe a
+boolMaybe True  x = Just x
+boolMaybe False _ = Nothing
+
+
+main :: IO ()
+main = do
+    ci@CabalInit{..} <- execParser opts
+    shelly $ verbosely $ do
+
+        let installArgs = catMaybes [ boolMaybe enableTests     "--enable-tests"
+                                    , boolMaybe enableProfiling "--enable-library-profiling"
+                                    , boolMaybe enableCoverage  "--enable-library-coverage"
+                                    ]
+        when deleteSandbox $ do
+            cabalSandbox_ "delete" [] >> cabalSandbox_ "init" []
+            dist_exists <- test_d "dist"
+            when dist_exists $ rm_rf "dist"
+        cabal_ "install"   ["--only-dependencies"]
+        cabal_ "configure" installArgs
+        cabal_ "build"     []
+        when enableTests $ cabal_ "test" []
+
+    where opts' =   CabalInit
+                <$> switch (short 't' <> long "enable-tests"     <> help "Enable tests in tests.")
+                <*> switch (short 'p' <> long "enable-profiling" <> help "Enable profiling in configuration.")
+                <*> switch (short 'c' <> long "enable-coverage"  <> help "Enable coverage in configuration.")
+                <*> switch (short 'd' <> long "delete"           <> help "Delete the sandbox before building.")
+          opts  = info opts' (  fullDesc
+                             <> progDesc "Initialize the cabal sandbox environment."
+                             <> header "cabal-init - a utility to initialize a cabal project."
+                             )
+
+
+data CabalInit = CabalInit
+               { enableTests     :: Bool
+               , enableProfiling :: Bool
+               , enableCoverage  :: Bool
+               , deleteSandbox   :: Bool
+               } deriving (Show)
+
