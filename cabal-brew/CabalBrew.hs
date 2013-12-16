e69c0b428792a871f1d4bd054d9ef8ce8d39d74d
Eric Rochester <erochest@virginia.edu>
2013-12-16 12:20:23 -0500

    Replicated original `cabal-brew` script.

--- cabal-brew/CabalBrew.hs
+++ cabal-brew/CabalBrew.hs
@@ -4,26 +4,64 @@
 module Main where
 
 
+import           Data.Text                 (Text)
+import qualified Data.Text                 as T
+import qualified Filesystem.Path.CurrentOS as FS
 import           Options.Applicative
-import           Prelude             hiding (FilePath)
+import           Prelude                   hiding (FilePath)
 import           Shelly
 
 
-type ProjectName    = String
-type ProjectVersion = String
+type ProjectName    = Text
+type ProjectVersion = Text
 
 
+brew_ :: Text -> [Text] -> Sh ()
+brew_ = command1_ "brew" []
+
+cabal_ :: Text -> [Text] -> Sh ()
+cabal_ = command1_ "cabal" []
+
+cabalBrew :: ProjectName -> ProjectVersion -> Sh ()
+cabalBrew name version = do
+    whenM (test_d sandbox) $ do
+        echo $ "Cleaning out old keg for " <> keg
+        brew_ "unlink" [keg]
+        rm_rf sandbox
+    chdir "/tmp" $ do
+        whenM (test_f "cabal.sandbox.config") $ rm "cabal.sandbox.config"
+        echo $ "cabal " <> name <> "-" <> version <> " => " <> toTextIgnore sandbox
+        cabal_ "sandbox" ["init", "--sandbox=" <> toTextIgnore sandbox]
+        cabal_ "install" [name <> "-" <> version]
+        brew_ "link" ["--overwrite", keg]
+    where keg     = "cabal-" <> name
+          sandbox = FS.concat [ "/usr"
+                              , "local"
+                              , "Cellar"
+                              , fromText keg
+                              , fromText version
+                              ]
 
 main :: IO ()
-main = execParser opts >>= print
+main = do
+    cfg <- execParser opts
+    shelly $ verbosely $ do
+        cabalBrew (projectName cfg) (projectVersion cfg)
+
     where opts' =   CB
-                <$> strOption (metavar "NAME"    <> help "The name of the package to install.")
-                <*> strOption (metavar "VERSION" <> help "The version to install.")
+                <$> textArg (metavar "NAME"    <> help "The name of the package to install.")
+                <*> textArg (metavar "VERSION" <> help "The version to install.")
           opts  = info (helper <*> opts') (  fullDesc
                                           <> progDesc "This installs a Haskell program\
                                                       \ to be managed by Homebrew."
                                           <> header "cabal-brew - install Haskell\
                                                     \ packages programs.")
+
+textArg :: Mod ArgumentFields Text -> Parser Text
+textArg = argument (Just . T.pack)
+
+textOption :: Mod OptionFields Text -> Parser Text
+textOption fields = nullOption (reader (pure . T.pack) <> fields)
 
 data CabalBrew = CB
                { projectName    :: ProjectName
