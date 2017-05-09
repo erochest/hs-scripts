f737297f59ca5f62e906ebfadf0e14dfd772e46a
Eric Rochester <eric.rochester@willowtreeapps.com>
2017-05-09 14:59:57 -0400

    Revert "wip"
    This reverts commit 2d3e2ff687072a866806c5b2cbad15ef73e268ca.

--- random-sample/RandomSample.hs
+++ random-sample/RandomSample.hs
@@ -4,32 +4,30 @@
 module Main where
 
 
--- import           Conduit
--- import           Control.Applicative
--- import           Control.Monad
--- import           Control.Monad.Trans.State.Strict
--- import qualified Data.ByteString.Lazy.Char8       as B
-import qualified Data.HashMap.Strict as M
-import qualified Data.List           as L
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Trans.State.Strict
+import qualified Data.ByteString.Lazy.Char8       as B
+import qualified Data.HashMap.Strict              as M
+import qualified Data.List                        as L
 import           Data.Maybe
 import           Data.Ord
 import           Data.Traversable
 import           System.Environment
--- import           System.Random.MWC
+import           System.Random.Mersenne.Pure64
 
 
 type LineCache a = M.HashMap Int a
 
 type RandomPair  = (Double, Double)
--- type PairState   = (PureMT, Int)
-type PairState = ((), Int)
+type PairState   = (PureMT, Int)
 type InputData a = (Int, RandomPair, a)
 
 
 randomPairs :: PairState -> a -> (PairState, InputData a)
-randomPairs (_g0, n) x =
-    let (r0, _g1) = undefined  -- randomDouble g0
-        (r1, g2) = undefined  -- randomDouble g1
+randomPairs (g0, n) x =
+    let (r0, g1) = randomDouble g0
+        (r1, g2) = randomDouble g1
     in  ((g2, succ n), (n, (r0, r1), x))
 
 inSample :: Double -> InputData a -> Bool
@@ -38,7 +36,7 @@ inSample n (i, (j, _), _) = j <= (n / fromIntegral i)
 swapKeys :: LineCache a -> Int -> Int -> a -> LineCache a
 swapKeys m k1 k2 v = M.insert k2 v $ M.delete k1 m
 
-sample :: () -> Int -> [a] -> [a]
+sample :: PureMT -> Int -> [a] -> [a]
 sample g k xs = map snd
               . L.sortBy (comparing fst)
               . M.toList
@@ -53,6 +51,7 @@ sample g k xs = map snd
             where ks = M.keys m
                   rm = ks !! truncate (j * k')
 
+
 readInt :: String -> Int
 readInt = read
 
@@ -61,8 +60,7 @@ main :: IO ()
 main = do
     args <- fmap readInt . listToMaybe <$> getArgs
     case args of
-        Just _k  -> do
-          -- s <- runResourceT $ (lineC stdinC) `fuseBoth` sourceRandom $$ undefined -- foldlC _f _s
-          undefined
-          -- B.interact (B.unlines . sample g k . B.lines)
+        Just k  -> do
+            g <- newPureMT
+            B.interact (B.unlines . sample g k . B.lines)
         Nothing -> putStrLn "usage: randomSample N"
